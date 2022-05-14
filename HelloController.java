8f9c7031cca59af0d458b46b0d6dd2ac512c1b98
SebastianSeczkowski <37137@studentwsei.pl>
2022-05-14 16:25:31 +0200

    Update HelloController.java
    Dodanie kliknięcia

--- HelloController.java
+++ HelloController.java
@@ -1,19 +1,21 @@
-package com.example.demo3;
-
+package com.example.lab2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    public Button btnMain;
 
-    public void onBtnAction(ActionEvent actionEvent) {
+    public Button btnMain;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent){
         System.out.println("Kliknięcie nr " + ++clickCounter);
-        if (clickCounter == 1)
-                btnMain.setText("I jeszcze raz");
-        }
+     
     }
+
+
     @FXML
     private Label welcomeText;
 
